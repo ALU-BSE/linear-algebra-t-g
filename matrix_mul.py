e655ee5413ae2542b37a58680083b3fdc61d3a9f
IrakizaGaius <g.irakiza@alustudent.com>
2025-01-20 22:18:29 +0200

    Add comments to clarify matrix multiplication logic in matrix_mul.py

--- matrix_mul.py
+++ matrix_mul.py
@@ -12,8 +12,11 @@ Ans = []
 for i in range(len(Prices)):
     row_sum = 0
     for j in range(len(Prices[0])):
+        # For each element in the array, multiply it by the corresponding element in the second array
+        # and add it to the row_sum
         row_sum += Prices[i][j]*Array2[j]
         pass
+    # Append the row_sum to the answer array
     Ans.append(row_sum)
     
 #Printing the result
