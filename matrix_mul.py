40a16f02a77b778e81468548a062fa54f1c6c90b
ktanguy <t.kwizera@alustudent.com>
2025-01-20 22:13:27 +0200

    Implement matrix multiplication logic in matrix_mul.py

--- matrix_mul.py
+++ matrix_mul.py
@@ -12,8 +12,9 @@ Ans = []
 for i in range(len(Prices)):
     row_sum = 0
     for j in range(len(Prices[0])):
-        # COMPLETE THE MISSING LOGIC HERE
+        row_sum += Prices[i][j]*Array2[j]
         pass
-
-
+    Ans.append(row_sum)
+    
+#Printing the result
 print(Ans)
